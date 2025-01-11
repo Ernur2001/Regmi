872982a2793ec10c1b376a97408c4f7ddc1eab12
Ernur <79253583+Ernur2001@users.noreply.github.com>
2025-01-11 19:47:22 +0500

    Delete static directory

--- static/js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let burger = document.querySelector(".header__burger");
-    burger.addEventListener("click", function(event) {
-      burger.classList.toggle("active");
-      document.querySelector(".header__menu").classList.toggle("active");
-      document.querySelector("body").classList.toggle("lock");
-    });
-  });
